97db418a21a5742fafff72940f75e0e61218383a
Oste Jannick <oste.jannick@hotmail.com>
2022-03-19 12:48:05 +0100

    GET example type fixed

--- src/net/views/GetExample.ts
+++ src/net/views/GetExample.ts
@@ -5,16 +5,19 @@ import { ViewTemplates } from "../ViewTemplates";
 @ViewTemplates.set
 export class GetExample
 {
-  get = (req: Request, res: Response, next: NextFunction): IWebResponse => {
+  get(req: Request, res: Response, next: NextFunction): IWebResponse 
+  {
     const index: number = Number.parseInt(`${(req as any).query.index}`);
-
-    return {
-        data: {
-            person: isNaN(index) ? "No person index defined" : [{name: "Jannick Oste"}, {name: "Tom Bom"}][index],
-            index: isNaN(index) ? -1 : index
-        }
+    const response: IWebResponse = {
+      data:
+      {
+        person: isNaN(index) ? "No person index defined" : [{name: "Jannick Oste"}, {name: "Tom Bom"}][index],
+        index: isNaN(index) ? -1 : index
+      }
     }
-  };
 
-  post =  undefined
-} 
+    return response;
+  }
+
+  post = undefined
+}
